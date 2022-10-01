87122e1ab58368aac8de1bb74aa35dab065a2f0b
Julia Ch <juliecha@yandex.ru>
2022-10-02 00:56:17 +0300

    Add Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите еще одно целое число");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n < m) 
+{
+    Console.WriteLine("Максимальное число = " + m);
+    Console.WriteLine("Минимальное число = " + n);
+}
+else if (m == n) Console.WriteLine("Введенные числа равны");
+else 
+{
+    Console.WriteLine("Максимальное число = " + n);
+    Console.WriteLine("Минимальное число = " + m);
+}
