c66c7031e4210e377cadd76352a554ffbe55e1eb
Julia Ch <juliecha@yandex.ru>
2022-10-02 01:23:22 +0300

    Add Ex006

---
+++ Ex006/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Проверим является ли число четным: введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n%2 == 0) Console.WriteLine("да, введенное число четное");
+else Console.WriteLine("нет, введенное число нечетное");
