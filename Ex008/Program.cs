56752a1a17eee226f894e77b35d7fc149553419a
Julia Ch <juliecha@yandex.ru>
2022-10-02 02:15:20 +0300

    UPD Ex008

--- Ex008/Program.cs
+++ Ex008/Program.cs
@@ -1,8 +1,30 @@
 ﻿Console.WriteLine("введите целое число");
 int n = Convert.ToInt32(Console.ReadLine());
 int i = 1;
-while (i <= n)
+if (n > i)
 {
-    if (i%2 == 0) Console.Write(i + " ");
-    i++;
+    while (i <= n)
+    {
+        if (i%2 == 0) Console.Write(i + " ");
+        i++;
+    }
 }
+else if (n == 0 | n == i) Console.WriteLine("Четных чисел нет");
+else // n < 0
+{
+    while (n < i)
+    {
+        if (n%2 == 0 & n!=0) Console.Write(n + " ");
+        n++;
+    }    
+}
+
+
+// Console.WriteLine("введите целое число");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// while (i <= n)
+// {
+//     if (i%2 == 0) Console.Write(i + " ");
+//     i++;
+// }
