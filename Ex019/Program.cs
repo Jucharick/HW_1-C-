5f4d91c592e111fa20950be3e5e492ba0299c461
Julia Ch <juliecha@yandex.ru>
2022-10-09 21:40:22 +0300

    UPD palindrome

--- Ex019/Program.cs
+++ Ex019/Program.cs
@@ -5,32 +5,53 @@
 // 23432 -> да
 
 
-Console.WriteLine("Введите пятизначное число");
+Console.WriteLine("Давайте проверим является ли число палиндромом. Введите целое число: ");
 string n = Console.ReadLine();
-int length = n.Length;
-
-if (length % 2 != 0)
+if ( int.TryParse(n, out int result) ) // проверя, что ввели число
 {
-    int i = 0;
-    if (n[i] == n[n.Length - 1 - i])
+    int length = n.Length;
+    if (length % 2 != 0)
     {
-        i++;
-        Console.WriteLine("палиндром!!!");
+        int i = 0;
+        bool check = false;
+        while (i < length/2)
+        {
+            if (n[i] == n[n.Length - 1 - i]) 
+            {
+                check = true;
+            }
+            else 
+            {
+                check = false;
+                break;
+            }
+            i++;
+        }   
+        if (check == true) Console.WriteLine("палиндром!!!");
+        else Console.WriteLine("нет, не палиндром");
     }
+    else Console.WriteLine("У палиндрома должно быть нечетное количество цифр");
 }
-else Console.WriteLine("нет, не палиндром");
+else Console.WriteLine("вы ввели некорректное значение");
 
 
-// Console.WriteLine("Введите число");
+
+
+
+
+// Console.WriteLine("Введите пятизначное число");
 // string n = Console.ReadLine();
 // int length = n.Length;
-
-// if (length % 2 != 0)
+// if (length == 5)
 // {
-//     for (int i = 0; i < length / 2; i++)
+//     if (n[0] == n[n.Length - 1])
 //     {
-//         if (n[i] == n[n.Length - 1 - i]) Console.WriteLine("палиндром!!!");
-//         if (n[i] != n[n.Length - 1 - i]) Console.WriteLine("нет");
+//         if (n[1] == n[n.Length - 2])
+//         {
+//             Console.WriteLine("палиндром!!!");
+//         }
+//         else Console.WriteLine("нет, не палиндром");
 //     }
+//     else Console.WriteLine("нет, не палиндром");
 // }
-// else Console.WriteLine("нет, не палиндром");
+// else Console.WriteLine("это не пятизначное число");
