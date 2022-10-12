202c482289a9340085ba010fb74756f260854bf0
Julia Ch <juliecha@yandex.ru>
2022-10-12 22:04:05 +0300

    Add Ex0027 - sumOfNumbers

---
+++ Ex027/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.WriteLine("Введите целое число:");
+string n = Console.ReadLine();
+int length = n.Length;
+
+int sumOfNumbers (int sum)
+{
+    sum = 0;
+    for (int index = 0; index < length; index++)
+    {
+        int num = int.Parse(Convert.ToString(n[index]));
+        sum = sum + num;
+    }
+    return sum;
+}
+
+Console.WriteLine($"сумма чисел числа {n} равно: {sumOfNumbers(int.Parse(n))}");
