9eb8bc0c1e69109429b050279edc344eec58e1d2
Julia Ch <juliecha@yandex.ru>
2022-10-20 21:24:48 +0300

    done Ex043

--- Ex043/Program.cs
+++ Ex043/Program.cs
@@ -28,5 +28,5 @@ double k2 = Convert.ToDouble(Console.ReadLine());
 // y = k2 * x + b2
 
 double x = (b2 - b1)/(k1 - k2);
-Console.WriteLine(x);
-
+double y = k2 * x + b2;
+Console.WriteLine($"Координаты точки пересечения двух прямых: ({Math.Round(x, 1)}, {Math.Round(y, 1)})");
