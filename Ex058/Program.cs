b2aac14565a0a5ef87db5bed84f7ca78e0a353e5
Julia Ch <juliecha@yandex.ru>
2022-10-26 22:49:32 +0300

    UPD-2  Ex058

--- Ex058/Program.cs
+++ Ex058/Program.cs
@@ -22,62 +22,31 @@
 // В произведении матриц АВ число строк равно числу строк матрицы А , а число столбцов равно числу столбцов матрицы В.
 
 
-int[,] matrixA = new int[,]
-{
-    {1, 4},
-    {3, 5},
-};
+int[,] matrixA = new int[2, 3];
 
-int[,] matrixB = new int[,]
-{
-    {3, 5},
-    {1, 2},
-};
+int[,] matrixB = new int[3, 4];
 
-int[,] matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
 
-int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB, int[,] matrixC)
+FillMatrix(matrixA, 0, 3);
+Console.WriteLine("matrixA");
+PrintMatrix(matrixA);
+Console.WriteLine();
+FillMatrix(matrixB, 1, 4);
+Console.WriteLine("matrixB");
+PrintMatrix(matrixB);
+Console.WriteLine("matrixA * matrixB");
+MatrixMultiplication(matrixA, matrixB);
+
+void FillMatrix(int[,] matr, int from, int to)
 {
-    for (int i = 0; i < matrixA.GetLength(0); i++)
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        for (int j = 0; j < matrixB.GetLength(1); j++)
+        for (int j = 0; j < matr.GetLength(1); j++)
         {
-            matrixC[i, j] = matrixA[i, j] * matrixB[i, j] + matrixA[i, j+1] * matrixB[i+1, j];
+            matr[i, j] = new Random().Next(from, to);
         }
     }
-    return matrixC;
 }
-PrintMatrix(matrixC);
-
-
-
-
-
-
-
-// Console.WriteLine("matrixA");
-// int[,] matrixA = new int[3, 4];
-// Console.WriteLine("matrixB");
-// int[,] matrixB = new int[2, 3];
-
-
-// FillMatrix(matrixA, 0, 11);
-// PrintMatrix(matrixA);
-// Console.WriteLine();
-// FillMatrix(matrixB, -3, 6);
-// PrintMatrix(matrixB);
-// MatrixMultiplication(matrixA, matrixB);
-
-// void FillMatrix(int[,] matr, int from, int to)
-// {
-//     for (int i = 0; i < matr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matr.GetLength(1); j++)
-//         {
-//             matr[i, j] = new Random().Next(from, to);
-//         }
-//     }
-// }
 
 void PrintMatrix(int[,] matr)
 {
@@ -91,34 +60,71 @@ void PrintMatrix(int[,] matr)
     }
 }
 
-// void MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+void MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+{
+    if (matrixA.GetLength(0) == matrixB.GetLength(1)) // число строк матрицы A равно числу столбцов матрицы B 
+    {
+        int[,] matrixC = new int[matrixB.GetLength(0), matrixA.GetLength(1)]; // матрица C - число строк равно числу строк матрицы В, а число столбцов равно числу столбцов матрицы A
+        for (int i = 0; i < matrixC.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrixC.GetLength(1); j++)
+            {
+                matrixC[i, j] = 0;
+                for (int k = 0; k < matrixB.GetLength(1); k++)
+                {
+                    matrixC[i, j] = matrixC[i, j] +  matrixB[i, k]*  matrixA[k, j];
+                }
+            }
+        }
+        PrintMatrix(matrixC);
+    }
+    else if (matrixA.GetLength(1) == matrixB.GetLength(0)) // число столбцов матрицы A равно числу строк B
+    {
+        int[,] matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)]; // матрица C - число строк равно числу строк матрицы А, а число столбцов равно числу столбцов матрицы В
+        for (int i = 0; i < matrixA.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrixB.GetLength(1); j++)
+            {
+                matrixC[i, j] = 0;
+                for (int k = 0; k < matrixA.GetLength(1); k++)
+                {
+                    matrixC[i, j] = matrixC[i, j] +  matrixA[i, k]*  matrixA[k, j];
+                }
+            }
+        }
+        PrintMatrix(matrixC);
+    }
+    else 
+    {
+        Console.WriteLine("matrixA нельзя умножить на matrixB");
+    }  
+}
+
+
+
+
+
+// int[,] matrixA = new int[,]
 // {
-//     if (matrixA.GetLength(0) == matrixB.GetLength(1)) // число строк матрицы A равно числу столбцов матрицы B 
-//     {
-//         int[,] matrixC = new int[matrixB.GetLength(0), matrixA.GetLength(1)]; // матрица C - число строк равно числу строк матрицы В, а число столбцов равно числу столбцов матрицы A
-//         for (int i = 0; i < matrixC.GetLength(0); i++)
-//         {
-//             for (int j = 0; j < matrixC.GetLength(1); j++)
-//             {
-//                 matrixC[i, j] = matrixA[i, j] * matrixB[i, j] + matrixA[i, j+1] * matrixB[i+1, j];
-//             }
-//         }
-//         PrintMatrix(matrixC);
-//     }
-//     else if (matrixA.GetLength(1) == matrixB.GetLength(0)) // число столбцов матрицы A равно числу строк B
-//     {
-//         int[,] matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)]; // матрица C - число строк равно числу строк матрицы А, а число столбцов равно числу столбцов матрицы В
-//         for (int i = 0; i < matrixC.GetLength(0); i++)
-//         {
-//             for (int j = 0; j < matrixC.GetLength(1); j++)
-//             {
-//                 matrixC[i, j] = matrixA[i, j] * matrixB[i, j] + matrixA[i, j+1] * matrixB[i+1, j];
-//             }
-//         }
-//         PrintMatrix(matrixC);
-//     }
-//     else 
-//     {
-//         Console.WriteLine("matrixA нельзя умножить на matrixB");
-//     }  
+//     {1, 4},
+//     {3, 5},
+// };
+
+// int[,] matrixB = new int[,]
+// {
+//     {3, 5},
+//     {1, 2},
+// };
+
+// int[,] matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)]; // число столбцов матрицы А совпадает с числом строк матрицы В
+
+// int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB, int[,] matrixC)
+// {
+//     matrixC[0, 0] = matrixA[0, 0] * matrixB[0, 0] + matrixA[0, 1] * matrixB[1, 0];
+//     matrixC[0, 1] = matrixA[0, 0] * matrixB[0, 1] + matrixA[0, 1] * matrixB[1, 1];
+//     matrixC[1, 0] = matrixA[1, 0] * matrixB[0, 0] + matrixA[1, 1] * matrixB[1, 0];
+//     matrixC[1, 1] = matrixA[1, 0] * matrixB[0, 1] + matrixA[1, 1] * matrixB[1, 1];
+//     return matrixC;
 // }
+// MatrixMultiplication(matrixA, matrixB, matrixC);
+// PrintMatrix(matrixC);
