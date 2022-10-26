27701383b048e0aa300e4f15a9e17367e31fb3ba
Julia Ch <juliecha@yandex.ru>
2022-10-26 19:23:27 +0300

    Add Fill and Print

--- Ex0541/Program.cs
+++ Ex0541/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ // Задайте двумерный массив. Напишите программу, которая упорядочит элементы по убыванию (или возрастанию).
+
+
+ Console.WriteLine("Введите высоту матрицы (количество строк)");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите длину матрицы (количество столбцов)");
+int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[m, n];
+
+FillMatrix(matrix, 0, 11);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+
+void FillMatrix(int[,] matr, int from, int to)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(from, to);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
