6301833ca2361422b558e4a1b9472f2603ec4ebc
Julia Ch <juliecha@yandex.ru>
2022-10-26 20:57:40 +0300

    UPD  Ex058

--- Ex058/Program.cs
+++ Ex058/Program.cs
@@ -9,7 +9,8 @@
 // 7 13
 // 14 25
 
-// Произведением двух матриц А и В называется матрица С, элемент которой, находящийся на пересечении i-й строки и j-го столбца, равен сумме произведений элементов i-й строки матрицы А на соответствующие (по порядку) элементы j-го столбца матрицы В.
+// Произведением двух матриц А и В называется матрица С, элемент которой, находящийся на пересечении i-й строки и j-го столбца, равен сумме произведений 
+// элементов i-й строки матрицы А на соответствующие (по порядку) элементы j-го столбца матрицы В.
 
 // С (0,0) = А(0,0) * B(0,0) + A(0,1) * B(1,0) = 1 · 3 + 4 · 1 = 3 + 4 = 7
 // С (0,1) = А(0,0) * B(0,1) + A(0,1) * B(1,1) = 1 · 5 + 4 · 2 = 5 + 8 = 13
@@ -19,3 +20,69 @@
 // Произведение двух матриц АВ имеет смысл только в том случае, когда число столбцов матрицы А совпадает с числом строк матрицы В.
 
 // В произведении матриц АВ число строк равно числу строк матрицы А , а число столбцов равно числу столбцов матрицы В.
+
+Console.WriteLine("matrixA");
+int[,] matrixA = new int[3, 4];
+Console.WriteLine("matrixB");
+int[,] matrixB = new int[2, 3];
+
+
+FillMatrix(matrixA, 0, 11);
+PrintMatrix(matrixA);
+Console.WriteLine();
+FillMatrix(matrixB, -3, 6);
+PrintMatrix(matrixB);
+MatrixMultiplication(matrixA, matrixB);
+
+
+
+void FillMatrix(int[,] matr, int from, int to)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(from, to);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+{
+    if (matrixA.GetLength(0) == matrixB.GetLength(1))
+    {
+        int[,] matrixC = new int[matrixA.GetLength(0), matrixA.GetLength(1)];
+    }
+    else if (matrixA.GetLength(1) == matrixB.GetLength(0))
+    {
+        int[,] matrixC = new int[matrixB.GetLength(0), matrixB.GetLength(1)];
+    }
+    else 
+    {
+        Console.WriteLine("matrixA нельзя умножить на matrixB");
+        return;
+    }
+    
+    for (int i = 0; i < matrixC.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixC.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+    PrintMatrix(matrixC);
+}
