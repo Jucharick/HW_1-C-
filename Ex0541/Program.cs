fe1228e1fa23f8e5413ad9f7aea32f846d910c27
Julia Ch <juliecha@yandex.ru>
2022-10-29 10:42:01 +0300

    UPD Ex0541

--- Ex0541/Program.cs
+++ Ex0541/Program.cs
@@ -19,7 +19,8 @@ else
     PrintMatrix(matrix);
     Console.WriteLine();
     SortMatrix(matrix);
-    // PrintMatrix(matrix);
+    Console.WriteLine("Матрица с упорядоченными элементами по возрастанию:");
+    PrintMatrix(matrix);
 }
 
 
@@ -46,47 +47,88 @@ void PrintMatrix(int[,] matr)
     }
 }
 
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        Console.Write($"{array[i]}; ");
-    }
-    Console.Write($"{array[array.Length - 1]}");
-    Console.WriteLine();
-}
-
-void SortArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        for (int j = 0; j <= array.Length - 2; j++)
-        {
-            if (array[j] > array[j+1])
-            {
-                int temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-            }
-        } 
-    }
-}
-
 void SortMatrix(int[,] matr)
 {
-    int[] arrayMatrElements = new int[matr.GetLength(0) * matr.GetLength(1)];
-    int arEl =0;
     for (int i = 0; i < matr.GetLength(0); i++)
     {
-        for (int j = 0; j < matr.GetLength(1); j++)
+        for (int k = 0; k <= matr.GetLength(0) - 2; k++)
         {
-            arrayMatrElements[arEl] = matr[i, j];
-            arEl++;
+            for (int j = 0; j < matr.GetLength(1); j++)
+            {
+                if (matr[k,j] > matr[k+1,j])
+                {
+                    int temp0 = matr[k,j];
+                    matr[k,j] = matr[k+1,j];
+                    matr[k+1,j] = temp0;
+                    for (int l = 0; l <= matr.GetLength(1)-2; l++)
+                    {
+                        if (matr[i,l] > matr[i,l+1])
+                        {
+                            int temp1 = matr[i,l];
+                            matr[i,l] = matr[i,l+1];
+                            matr[i,l+1] = temp1;
+                        }
+                    }
+                }
+            }
         }
     }
-    PrintArray(arrayMatrElements);
-    SortArray(arrayMatrElements);
-    PrintArray(arrayMatrElements);
-    
 }
+
+
+
+
+
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         Console.Write($"{array[i]}; ");
+//     }
+//     Console.Write($"{array[array.Length - 1]}");
+//     Console.WriteLine();
+// }
+
+// void SortArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         for (int j = 0; j <= array.Length - 2; j++)
+//         {
+//             if (array[j] > array[j+1])
+//             {
+//                 int temp = array[j];
+//                 array[j] = array[j+1];
+//                 array[j+1] = temp;
+//             }
+//         } 
+//     }
+// }
+
+// void SortMatrix(int[,] matr)
+// {
+//     int[] arrayMatrElements = new int[matr.GetLength(0) * matr.GetLength(1)];
+//     int arEl = 0;
+//     for (int i = 0; i < matr.GetLength(0); i++) // преобразую матрицу в массив
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             arrayMatrElements[arEl] = matr[i, j];
+//             arEl++;
+//         }
+//     }
+//     // PrintArray(arrayMatrElements);
+//     SortArray(arrayMatrElements); // сортирую массив с элементами матрицы
+//     // PrintArray(arrayMatrElements);
+//     int mrEl =0;
+//     for (int i = 0; i < matr.GetLength(0); i++) // преобразую отсортированный массив обратно в матрицу
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i, j] = arrayMatrElements[mrEl];
+//             mrEl++;
+//         }
+//     }   
+// }
 
