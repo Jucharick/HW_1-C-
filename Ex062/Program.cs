674971c12f06e950b991f6e56447b3ab240a2afe
Julia Ch <juliecha@yandex.ru>
2022-10-31 20:26:45 +0300

    Add Ex062

---
+++ Ex062/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+
+int[,] matrix = new int[4, 4]
+                {
+                    {16,  2, 10,  4},
+                    {5,  6,  11,  8},
+                    {9,  3,  7,  12}, 
+                    {13, 14, 15,  1},
+                };
+
+
+SortBySpiral(matrix);
+PrintMatrix(matrix);
+
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortBySpiral(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            
+        }
+        
+    }
+}
