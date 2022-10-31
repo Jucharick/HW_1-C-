49a53d9b0a10515f47e396e8124829223781fd6c
Julia Ch <juliecha@yandex.ru>
2022-10-31 22:34:09 +0300

    UPD Ex057

--- Ex057/Program.cs
+++ Ex057/Program.cs
@@ -8,3 +8,50 @@
 // 3 встречается 1 раз
 // 4 встречается 1 раз
 // 6 встречается 2 раза
+
+
+Console.WriteLine("Введите высоту матрицы (количество строк)");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите длину матрицы (количество столбцов)");
+int n = int.Parse(Console.ReadLine());
+
+
+if (m <= 0 || n <= 0)
+{
+    Console.WriteLine("Количество строк и/или количество столбцов матрицы не может быть меньше или равно 0");
+}
+else
+{
+    int[,] matrix = new int[m, n];
+
+    FillMatrix(matrix, 0, 11);
+    PrintMatrix(matrix);
+    Console.WriteLine();
+    SortMatrix(matrix);
+    Console.WriteLine("Матрица с упорядоченными элементами по возрастанию:");
+    PrintMatrix(matrix);
+}
+
+
+void FillMatrix(int[,] matr, int from, int to)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(from, to);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
