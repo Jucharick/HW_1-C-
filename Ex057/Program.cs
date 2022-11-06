d472fcca63158f37676fd265067af6c544d1c75c
Julia Ch <juliecha@yandex.ru>
2022-11-06 22:46:35 +0300

    done Ex057

--- Ex057/Program.cs
+++ Ex057/Program.cs
@@ -11,67 +11,100 @@
 
 
 Console.WriteLine("Введите высоту матрицы (количество строк)");
-int m = int.Parse(Console.ReadLine());
+int rows = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите длину матрицы (количество столбцов)");
-int n = int.Parse(Console.ReadLine());
+int columns = int.Parse(Console.ReadLine());
 
 
-if (m <= 0 || n <= 0)
+if (rows <= 0 || columns <= 0)
 {
     Console.WriteLine("Количество строк и/или количество столбцов матрицы не может быть меньше или равно 0");
 }
 else
 {
-    int[,] matrix = new int[m, n];
-
-    FillMatrix(matrix, 0, 6);
+    int [,] matrix  = CreateMatrixRndInt(rows, columns, 0, 6);
     PrintMatrix(matrix);
     Console.WriteLine();
-    Find(matrix);
 
+    int[] arrayFromMtrix= GetArrayFromMtrix(matrix);
+    Array.Sort(arrayFromMtrix);
+    PrintArray(arrayFromMtrix);
+    CountPrintArrayGlossary(arrayFromMtrix);
 }
 
 
-void FillMatrix(int[,] matr, int from, int to)
+
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
-    for (int i = 0; i < matr.GetLength(0); i++)
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) // 0 - rows
     {
-        for (int j = 0; j < matr.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++) // 1 - columns
         {
-            matr[i, j] = new Random().Next(from, to);
+            matrix[i, j] = rnd.Next(min, max + 1);
         }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int [,] matrix) 
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) // идем по строкам
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++) // идем по колонкам
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($" {matrix[i, j], 2} | "); // 2 - для выравнивания вывода матрицы выводим по 2 символа
+            else Console.Write($" {matrix[i, j], 2} ");
+        }
+        Console.WriteLine("|");
     }
 }
 
-void PrintMatrix(int[,] matr)
+void PrintArray(int[] array)
 {
-    for (int i = 0; i < matr.GetLength(0); i++)
+    for (int i = 0; i < array.Length - 1; i++)
     {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            Console.Write($" {matr[i, j]} ");
-        }
-        Console.WriteLine();
+        Console.Write($"{array[i]}; ");
     }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.WriteLine();
 }
 
-
-void Find(int[,] matr)
+int[] GetArrayFromMtrix(int[,] matrix)
 {
-    int[] array = new int[6]; //   встречаются элементы от 0 до 5 - 6 шт
-    for (int i = 0; i < matr.GetLength(0); i++)
+    int[] result = new int[matrix.Length]; // matrix.Length - показывает количество элементов в двумерном массиве
+    int index = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++) // 0 - rows
     {
-        for (int j = 0; j < matr.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++) // 1 - columns
         {
-            array[matr[i, j]] = array[matr[i, j]] + 1;
+            result[index] = matrix[i, j];
+            index++;
+        }
+    }
+    return result;
+}
 
-        }
-    }
-    for (int i = 0; i < array.Length; i++)
+void CountPrintArrayGlossary(int[] array)
+{
+    int num = array[0];
+    int counter = 1; // 1 потому что значение array[0] уже +1
+    for (int i = 1; i < array.Length; i++) // начинаем с 1 для того, чтобы не посчитать первый элемент (он у нас уже посчитан counter = 1)
     {
-        if (array[i] > 0)
+        if (num == array[i])
         {
-            Console.WriteLine($"{i} встречается {array[i]} раз(а)");
+            counter++;
+        }
+        else
+        {
+            Console.WriteLine($"{num} встречается {counter} раз(а)");
+            num = array[i];
+            counter = 1;
         }
     }
+    Console.WriteLine($"{num} встречается {counter} раз(а)"); // еще раз вывожу, потому что последнее число не попадает в else
 }
