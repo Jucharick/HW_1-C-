bd20159fb104ffb232bf6836a4644937b537ab66
Julia Ch <juliecha@yandex.ru>
2022-11-13 11:15:24 +0300

    done Ex066

--- Ex066/Program.cs
+++ Ex066/Program.cs
@@ -3,3 +3,16 @@
 // M = 4; N = 8. -> 30
 
 
+Console.WriteLine("Задайте start");
+int start = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте stop");
+int stop = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine($"Сумма натуральных элементов в промежутке от start до stop => {SumNaturalNumbers(start)}");
+
+int SumNaturalNumbers(int start)
+{
+    if (start < stop) return SumNaturalNumbers(start + 1) + start;
+    else return stop;
+}
