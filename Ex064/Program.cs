e76f11deeacf122d57f6e6fb6561bcae412cba60
Julia Ch <juliecha@yandex.ru>
2022-11-13 10:51:00 +0300

    done Ex064

--- Ex064/Program.cs
+++ Ex064/Program.cs
@@ -3,3 +3,31 @@
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 
+Console.WriteLine("Задайте значение N");
+int n = int.Parse(Console.ReadLine());
+
+PrintNaturalNumbers(n);
+Console.WriteLine();
+PrintNaturalNumbers1(1);
+
+void PrintNaturalNumbers(int n) // способ 1
+{
+    int number = 1;
+    if (number <= n)
+    {
+        Console.Write($"{n} "); // достаточно поменять местами вывод и рекурсионный вызов и вывод будет от N до 1
+        PrintNaturalNumbers(n - 1);
+    }
+}
+
+
+void PrintNaturalNumbers1(int number) // способ 2
+{
+
+    if (n == number) Console.Write(number);
+    else
+    {
+        PrintNaturalNumbers1(number + 1); // достаточно поменять местами вывод и рекурсионный вызов и вывод будет от N до 1
+        Console.Write($" {number}");
+    }
+}
